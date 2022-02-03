6d7adca9cdebeb5c847ca9fcb2368ba03fadca0d
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-02-03 10:01:59 -0500

    implement several navfac functions with unit tests

---
+++ src/navfacFunctions.js
@@ -0,0 +1,212 @@
+/**
+ * @module navfac
+ * 
+ * @description This file contains functions intended to be used for the design 
+ * of deep foundations using the NAVFAC design methodology. The design manual 
+ * can be downloaded at 
+ * https://web.mst.edu/~rogersda/umrcourses/ge441/DM7_02.pdf
+ * 
+ * The relevant design information begins on page 215 of the pdf.
+ * This is 7.2-193 within the context of the document.
+ */
+
+
+/** @constant
+ *  @type {number}
+ *  @description The unit weight of water in pounds per cubic foot (pcf), 62.4
+ */
+export const UNIT_WEIGHT_WATER = 62.4;
+
+/** @constant
+ *  @type {object}
+ *  @description Object to be used as a lookup table for granularNq().
+ *    This is defined as a global variable so we don't need to re-define it
+ *    each time we call granularNq().
+ * 
+ *    To use the table, use NQ_LOOKUP_TABLE[phi][index].
+ *    index === 0 for drilled pier, index === 1 for driven pile
+ *  @see granularNq
+ */
+export const NQ_LOOKUP_TABLE = {
+  26: [5, 10],
+  27: [6.5, 12.5],
+  28: [8, 15],
+  29: [9, 18],
+  30: [10, 21],
+  31: [12, 24],
+  32: [14, 29],
+  33: [17, 35],
+  34: [21, 42],
+  35: [25, 50],
+  36: [30, 62],
+  37: [38, 77],
+  38: [43, 86],
+  39: [60, 120],
+  40: [72, 145]
+}
+
+/**
+ *  @param {boolean} isDrilled true if drilled pier, false if driven pile
+ *  @param {number} phi an integer value, 26 to 40 inclusive, or 0 for clay
+ *  @returns {number} Nq value for the provided foundation type and phi
+ *  @description Returns the Bearing Capacity Factor Nq for a given internal
+ *    soil friction angle and foundation type (drilled or driven). Applicable
+ *    to granular soils only. Clay soils will have Nq of 0.
+ * 
+ *    Based on the Bearing Capacity Factors table on p.216.
+ *    Nq for phi === 27, 29 are linearly interpolated from given values.
+ */
+export function granularNq(isDrilled, phi) {
+  if (phi === 0) return 0;
+  const index = isDrilled ? 0 : 1;
+  return NQ_LOOKUP_TABLE[phi][index];
+}
+
+/**
+ *  @param {number} pounds weight of an object in pounds
+ *  @returns {number} weight of the object in kips
+ *  @description Converts a weight from pounds to kips. 1 kip = 1000 pounds
+ */
+export function poundsToKips(pounds) {
+  return pounds / 1000;
+}
+
+/**
+ * @param {number} skinFrictionCapacity The skin friction capacity in kips
+ * @param {number} endBearingCapacity The end bearing capacity in kips
+ * @returns {number} The ultimate load capacity in kips
+ * @description Calculates the ultimate load capacity as the sum of the skin 
+ *    friction and end bearing capacity. In a tension condition, the skin 
+ *    friction will be zero. The end bearing could only be zero if the pile 
+ *    was bearing in the ignored zone, which you shouldn't be bothering with
+ *    anyway.
+ * 
+ *    See p.215 for more details
+ */
+export function ultimateLoadCapacity(skinFrictionCapacity, endBearingCapacity) {
+  return skinFrictionCapacity + endBearingCapacity;
+}
+
+/**
+ * @param {number} internalPhi The internal friction angle as an integer
+ * @param {string} material Can be "TIMBER", "CONCRETE", or "STEEL"
+ * @returns {number} The contact friction angle for the given phi and material
+ * @description Calculates the contact friction angle for a given internal soil 
+ *    friction angle and material type. Formulas are given on p.216
+ */
+export function contactFrictionAngle(internalPhi, material) {
+  switch (material) {
+    case ("TIMBER"):
+      return 0.75 * internalPhi;
+    case ("CONCRETE"):
+      return 0.75 * internalPhi;
+    case ("STEEL"):
+      return 20;
+    default:
+      console.error(`An invalid material type has been provided within 
+      contactFrictionAngle. Defaulting to 0 for safety.`);
+      return 0;
+  }
+}
+
+/**
+ * @constant
+ * @type {object} 
+ * @description Lookup table to be used by horizontalEarthPressureCoefficient 
+ *  so that this doesn't need to be re-defined at each call. 
+ *  To use the table, use KH_LOOKUP_TABLE[pileType][index]
+ *  index should be 0 for compression, 1 for tension.
+ *  Values used are the averages from the potential range given on p.216
+ */
+export const KH_LOOKUP_TABLE = {
+  "DRIVEN-SINGLE-H-PILE": [0.75, 0.4],
+  "DRIVEN-SINGLE-DISPLACEMENT-PILE": [1.25, 0.8],
+  "DRIVEN-SINGLE-DISPLACEMENT-TAPERED-PILE": [1.75, 1.15],
+  "DRIVEN-JETTED-PILE": [0.65, 0.45],
+  "DRILLED-PILE": [0.7, 0.4],
+};
+
+/**
+ * @param {string} pileType A string value representing the pile type. 
+ *    Must be one of the following: 
+ *    [
+ *      "DRIVEN-SINGLE-H-PILE",
+ *      "DRIVEN-SINGLE-DISPLACEMENT-PILE",
+ *      "DRIVEN-SINGLE-DISPLACEMENT-TAPERED-PILE",
+ *      "DRIVEN-JETTED-PILE",
+ *      "DRILLED-PILE"
+ *    ]
+ * @param {boolean} isCompression true if compression, false if tension
+ * @returns {number} The horizontal earth pressure coefficient
+ * @description Returns the horizontal earth pressure coefficient for a given 
+ *    pile type depending on if it is in compression or tension. The values 
+ *    used are the average values in the potential range given in NAVFAC 
+ *    on p.216.
+ */
+export function horizontalEarthPressureCoefficient(pileType, isCompression) {
+  const index = isCompression ? 0 : 1;
+  return KH_LOOKUP_TABLE[pileType][index];
+}
+
+/**
+ * @param {number} c the cohesion in pounds per square foot (psf) of a clay
+ * @param {string} material the material used. Can take the values 
+ *  "TIMBER", "CONCRETE", or "STEEL"
+ * @returns {number} the adhesion in pounds per square foot (psf) of the clay
+ * @description Calculates the adhesion between a clay and the foundation 
+ *  material based on FIGURE 2 on p.219 using linear interpolation
+ */
+export function adhesion(c, material) {
+  switch (material) {
+    case ("TIMBER"):
+      if (c < 250) return c;
+      else if (c < 500) { //adhesion is in range 250-480
+        return 250 + (c - 250) / 250 * 230;
+      } else if (c < 1000) { //adhesion is in range 480-750
+        return 480 + (c - 500) / 500 * 270;
+      } else if (c < 2000) { //adhesion is in range 750-950
+        return 750 + (c - 1000) / 1000 * 200;
+      } else if (c < 4000) { //adhesion is in range 950-1300
+        return 950 + (c - 2000) / 2000 * 350;
+      } else return 1300;
+    case ("CONCRETE"):
+      if (c < 250) return c;
+      else if (c < 500) { //adhesion is in range 250-480
+        return 250 + (c - 250) / 250 * 230;
+      } else if (c < 1000) { //adhesion is in range 480-750
+        return 480 + (c - 500) / 500 * 270;
+      } else if (c < 2000) { //adhesion is in range 750-950
+        return 750 + (c - 1000) / 1000 * 200;
+      } else if (c < 4000) { //adhesion is in range 950-1300
+        return 950 + (c - 2000) / 2000 * 350;
+      } else return 1300;
+    case ("STEEL"):
+      if (c < 250) return c;
+      else if (c < 500) { //adhesion is in range 250-460
+        return 250 + (c - 250) / 250 * 210;
+      } else if (c < 1000) { //adhesion is in range 460-700
+        return 460 + (c - 500) / 500 * 240;
+      } else if (c < 2000) { //adhesion is in range 700-720
+        return 700 + (c - 1000) / 1000 * 20;
+      } else if (c < 4000) { //adhesion is in range 720-750
+        return 720 + (c - 2000) / 2000 * 30;
+      } else return 750;
+    default:
+      return 0;
+  }
+}
+
+/**
+ * @param {number} depth foundation embedment depth in feet
+ * @param {number} width foundation width in feet
+ * @returns {number} A number between 6.29 and 9
+ * @description Calculates the bearing capacity factor in cohesive soils. 
+ *  This assumes a cylindrical foundation. 
+ *  The equation used is a 4th order approximation of the curve shown in 
+ *  FIGURE 2 on p.219, which asymptotically approaches 9. In most cases,
+ *  this function will be returning 9.
+ */
+export function cohesiveNc(depth, width) {
+  let x = depth / width;
+  return (x < 4) ? 6.29 + 1.88*x - 0.506*x**2 + 0.0632*x**3 - 0.0031*x**4 : 9;
+}
---
+++ src/tests/navfacFunctions.test.js
@@ -0,0 +1,249 @@
+import '../navfacFunctions';
+import {
+  granularNq,
+  poundsToKips,
+  ultimateLoadCapacity,
+  contactFrictionAngle,
+  horizontalEarthPressureCoefficient,
+  adhesion,
+  cohesiveNc,
+} from '../navfacFunctions';
+
+// granularNq
+test('Drilled pier with phi=0 has Nq of 0', () => {
+  expect(granularNq(true, 0)).toBe(0);
+  expect(granularNq(false, 0)).toBe(0);
+})
+
+test('Drilled pier with phi=26 has Nq of 5', () => {
+  expect(granularNq(true, 26)).toBe(5);
+});
+
+test('Drilled pier with phi=40 has Nq of 72', () => {
+  expect(granularNq(true, 40)).toBe(72);
+});
+
+test('Driven pile with phi=26 has Nq of 10', () => {
+  expect(granularNq(false, 26)).toBe(10);
+});
+
+test('Driven pile with phi=40 has Nq of 145', () => {
+  expect(granularNq(false, 40)).toBe(145);
+});
+
+// poundsToKips
+test('1000 pounds is 1 kip', () => {
+  expect(poundsToKips(1000)).toBe(1);
+});
+
+test('6482 pounds is 6.482 kips', () => {
+  expect(poundsToKips(6482)).toBe(6.482);
+});
+
+test('0 pounds is 0 kips', () => {
+  expect(poundsToKips(0)).toBe(0);
+})
+
+test('982 pounds is .982 kips', () => {
+  expect(poundsToKips(982)).toBe(.982);
+})
+
+// ultimatLoadCapacity
+test(`Ultimate load capacity for 3 kips skin friction and 30 kips end 
+  bearing is 33 kips`, () => {
+  expect(ultimateLoadCapacity(3, 30)).toBe(33);
+});
+
+// contactFrictionAngle
+test(`Contact friction angle for TIMBER with phi=32 is 24`, () => {
+  expect(contactFrictionAngle(32, "TIMBER")).toBe(24);
+});
+
+test(`Contact friction angle for CONCRETE with phi=35 is 26.25`, () => {
+  expect(contactFrictionAngle(35, "CONCRETE")).toBe(26.25);
+});
+
+test(`Contact friction angle for STEEL with phi=40 is 20`, () => {
+  expect(contactFrictionAngle(40, "STEEL")).toBe(20);
+});
+
+test(`Contact friction angle for WOOD with phi=31 is 0`, () => {
+  expect(contactFrictionAngle(31, "WOOD")).toBe(0);
+});
+
+// horizontalEarthPressureCoefficient
+test(`hor. Earth pressure coef. for "DRIVEN-SINGLE-H-PILE" in compression 
+  should be 0.75`, () => {
+  expect(horizontalEarthPressureCoefficient("DRIVEN-SINGLE-H-PILE", true))
+    .toBe(0.75);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-SINGLE-H-PILE" in tension 
+  should be 0.4`, () => {
+  expect(horizontalEarthPressureCoefficient("DRIVEN-SINGLE-H-PILE", false))
+    .toBe(0.4);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-SINGLE-DISPLACEMENT-PILE" in 
+  compression should be 1.25`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRIVEN-SINGLE-DISPLACEMENT-PILE",
+    true)).toBe(1.25);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-SINGLE-DISPLACEMENT-PILE" in 
+  tension should be 0.8`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRIVEN-SINGLE-DISPLACEMENT-PILE",
+    false)).toBe(0.8);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-SINGLE-DISPLACEMENT-TAPERED-PILE" 
+  in compression should be 1.75`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRIVEN-SINGLE-DISPLACEMENT-TAPERED-PILE",
+    true)).toBe(1.75);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-SINGLE-DISPLACEMENT-TAPERED-PILE" 
+  in tension should be 1.15`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRIVEN-SINGLE-DISPLACEMENT-TAPERED-PILE",
+    false)).toBe(1.15);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-JETTED-PILE" 
+  in compression should be 0.65`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRIVEN-JETTED-PILE",
+    true)).toBe(0.65);
+});
+
+test(`hor. Earth pressure coef. for "DRIVEN-JETTED-PILE" 
+  in tension should be 0.45`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRIVEN-JETTED-PILE",
+    false)).toBe(0.45);
+});
+
+test(`hor. Earth pressure coef. for "DRILLED-PILE" 
+  in compression should be 0.7`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRILLED-PILE",
+    true)).toBe(0.7);
+});
+
+test(`hor. Earth pressure coef. for "DRILLED-PILE" 
+  in tension should be 0.4`, () => {
+  expect(horizontalEarthPressureCoefficient(
+    "DRILLED-PILE",
+    false)).toBe(0.4);
+});
+
+// adhesion
+test(`adhesion for clay with c=150 and "CONCRETE" 
+  should be 150`, () => {
+    expect(adhesion(150, "CONCRETE")).toBe(150);
+});
+
+test(`adhesion for clay with c=400 and "CONCRETE" 
+  should be 388`, () => {
+    expect(adhesion(400, "CONCRETE")).toBe(388);
+});
+
+test(`adhesion for clay with c=750 and "CONCRETE" 
+  should be 615`, () => {
+    expect(adhesion(750, "CONCRETE")).toBe(615);
+});
+
+test(`adhesion for clay with c=1850 and "CONCRETE" 
+  should be 920`, () => {
+    expect(adhesion(1850, "CONCRETE")).toBe(920);
+});
+
+test(`adhesion for clay with c=3100 and "CONCRETE" 
+  should be 1142.5`, () => {
+    expect(adhesion(3100, "CONCRETE")).toBe(1142.5);
+});
+
+test(`adhesion for clay with c=5000 and "CONCRETE" 
+  should be 1300`, () => {
+    expect(adhesion(5000, "CONCRETE")).toBe(1300);
+});
+
+test(`adhesion for clay with c=150 and "TIMBER" 
+  should be 150`, () => {
+    expect(adhesion(150, "TIMBER")).toBe(150);
+});
+
+test(`adhesion for clay with c=400 and "TIMBER" 
+  should be 388`, () => {
+    expect(adhesion(400, "TIMBER")).toBe(388);
+});
+
+test(`adhesion for clay with c=750 and "TIMBER" 
+  should be 615`, () => {
+    expect(adhesion(750, "TIMBER")).toBe(615);
+});
+
+test(`adhesion for clay with c=1850 and "TIMBER" 
+  should be 920`, () => {
+    expect(adhesion(1850, "TIMBER")).toBe(920);
+});
+
+test(`adhesion for clay with c=3100 and "TIMBER" 
+  should be 1142.5`, () => {
+    expect(adhesion(3100, "TIMBER")).toBe(1142.5);
+});
+
+test(`adhesion for clay with c=5000 and "TIMBER" 
+  should be 1300`, () => {
+    expect(adhesion(5000, "TIMBER")).toBe(1300);
+});
+
+test(`adhesion for clay with c=100 and "STEEL" 
+  should be 100`, () => {
+    expect(adhesion(100, "STEEL")).toBe(100);
+});
+
+test(`adhesion for clay with c=450 and "STEEL" 
+  should be 418`, () => {
+    expect(adhesion(450, "STEEL")).toBe(418);
+});
+
+test(`adhesion for clay with c=615 and "STEEL" 
+  should be 515.2`, () => {
+    expect(adhesion(615, "STEEL")).toBe(515.2);
+});
+
+test(`adhesion for clay with c=1550 and "STEEL" 
+  should be 711`, () => {
+    expect(adhesion(1550, "STEEL")).toBe(711);
+});
+
+test(`adhesion for clay with c=2700 and "STEEL" 
+  should be 730.5`, () => {
+    expect(adhesion(2700, "STEEL")).toBe(730.5);
+});
+
+test(`adhesion for clay with c=5000 and "STEEL" 
+  should be 750`, () => {
+    expect(adhesion(5000, "STEEL")).toBe(750);
+});
+
+// cohesiveNc
+test(`Nc for depth=20 and width=1 should be 9`, () => {
+  expect(cohesiveNc(20, 1)).toBe(9);
+});
+
+test(`Nc for depth=20 and width=5 should be 9`, () => {
+  expect(cohesiveNc(20, 5)).toBe(9);
+});
+
+test(`Nc for depth=0 and width=5 should be 6.29`, () => {
+  expect(cohesiveNc(0, 5)).toBe(6.29);
+});
+
+test(`Nc for depth=20 and width=8 is 8.69 (rounded to 2 places)`, () => {
+  expect(Number(cohesiveNc(20, 8).toFixed(2))).toBe(8.69);
+});
