b2ddd9eaf45fbd8381e3e14a13ba1f9a0686f731
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-02-07 22:24:21 -0500

    move LoginView into components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
 import WelcomeView from './components/welcome-view/welcomeView';
-import LoginView from './login-view/loginView';
+import LoginView from './components/login-view/loginView';
 import RegisterView from './components/register-view/registerView';
 
 function App() {
