6e3c26775adecc24f1051d0fcf3114c4ad0b269a
RCarpus <40838987+RCarpus@users.noreply.github.com>
2022-02-17 08:53:57 -0500

    convert to function component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,37 +9,32 @@ import { Link } from 'react-router-dom';
  * @description Navbar is pinned to the top of the screen at all times.
  * (See App component.) 
  * If the user is logged in, renders navigation links.
+ * @param {boolean} isLoggedIn true if the user is logged in, else false
  */
-export default class Navbar extends React.Component {
-
+export default function Navbar(props) {
   /**
    * Logs out the user by clearing localStorage, and reloads the page. 
    * This will cause them to be redirected to WelcomeView.
    * @param {object} e event object
    * @returns false
    */
-  logout(e) {
+  function logout(e) {
     e.preventDefault();
     localStorage.clear();
-    window.location.href='/';
+    window.location.href = '/';
     return false;
   }
 
-  render() {
-    const { isLoggedIn } = this.props;
-    return (
-      <div className="banner">
-
-        {this.props.isLoggedIn &&
-          <div className="banner__links">
-            <button className="banner__link" onClick={(e) => this.logout(e)}>Logout</button>
-            <Link className="banner__link" to="/home">Home</Link>
-          </div>
-        }
-        <h1 className="banner__title">NAVFAC Deep Foundation Design</h1>
-      </div>
-    )
-  }
-
-
+  const { isLoggedIn } = props;
+  return (
+    <div className="banner">
+      {isLoggedIn &&
+        <div className="banner__links">
+          <button className="banner__link" onClick={(e) => logout(e)}>Logout</button>
+          <Link className="banner__link" to="/home">Home</Link>
+        </div>
+      }
+      <h1 className="banner__title">NAVFAC Deep Foundation Design</h1>
+    </div>
+  )
 }
